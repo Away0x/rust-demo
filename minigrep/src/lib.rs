347619e8cf96e2a74ccb98ff05c7bb55560b24ec
WuTong <tong.wu@rocketg.com>
2021-08-12 22:12:04 +0800

    iter

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,6 +1,6 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
 
 pub struct Config {
     pub query: String,
@@ -9,16 +9,40 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    // pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    //     if args.len() < 3 {
+    //         return Err("not enough arguments");
+    //     }
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    //     let query = args[1].clone();
+    //     let filename = args[2].clone();
+
+    //     let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+    //     Ok(Config { query, filename, case_sensitive })
+    // }
+
+    // 使用迭代器优化版本
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next(); // 不使用第一个参数
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Self { query, filename, case_sensitive })
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
@@ -39,15 +63,21 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    // let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
 
-    results
+    // results
+
+    // 使用迭代器优化
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
