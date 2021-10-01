debcc07845f3d9f3340c3af9b5ffe2897d142ec0
WuTong <tong.wu@rocketg.com>
2021-10-01 21:39:13 +0800

    rust-demo

--- fs-demo/src/main.rs
+++
@@ -1,8 +0,0 @@
-use std::io::{BufRead, Read, Write, BufReader};
-fn main() -> std::io::Result<()> {
-    // 打开文件
-    let mut f = std::fs::OpenOptions::new().append(true).open("test.txt")?;
-
-    writeln!(&mut f, "hello world")?;
-    Ok(())
-}
