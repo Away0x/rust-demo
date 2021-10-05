5291fecd8850bfa1235e1b9f2cef0ddf75699d20
WuTong <tong.wu@rocketg.com>
2021-10-05 15:37:20 +0800

    queryer

---
+++ queryer/README.md
@@ -0,0 +1,6 @@
+# SQL 查询工具
+
+```bash
+cargo run --example dialect
+cargo run --example covid
+```
---
+++ queryer/queryer/examples/covid.rs
@@ -0,0 +1,20 @@
+use anyhow::Result;
+use queryer::query;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let url = "file://./covid.csv";
+
+    // 使用 sql 从 URL 里获取数据
+    let sql = format!(
+        "SELECT location name, total_cases, new_cases, total_deaths, new_deaths \
+        FROM {} where new_deaths >= 500 ORDER BY new_cases DESC",
+        url
+    );
+    let df1 = query(sql).await?;
+    println!("{:?}", df1);
+
+    Ok(())
+}
