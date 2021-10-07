0d8a37a9977a4e75670de4564e3d680a66621599
WuTong <tong.wu@rocketg.com>
2021-10-07 21:28:02 +0800

    libconfig

--- libconfig/src/conf/mod.rs
+++ libconfig/src/conf/mod.rs
@@ -1,8 +1,10 @@
-// 该文件引入了 conf mod 下需要共用的东西, 所以其他子文件中只需要 use super::*; 即可使用
-// 把该文件移动为 src/conf.rs 等同于 src/conf/mod.rs 的作用
-
-// pub(crate) 表示该 mod 的 pub 行为只在当前 crate 生效 == pub(in crate)
-// 同理还有 pub(super), pub(in super), pub(self), pub(in self)
+// 1. 该文件引入了 conf mod 下需要共用的东西, 所以其他子文件中只需要 use super::*; 即可使用
+// 2. 把该文件移动为 src/conf.rs 等同于 src/conf/mod.rs 的作用
+//    这里同名文件等于同名文件夹的导出入口的行为是在 edition=2018 引入的
+//    edition=2015 模块文件夹导出入口都为文件夹下的 mod.rs
+//    个人更喜欢 mod.rs 的形式, 类似 python __init__.py. 看起来无歧义, 很干净
+// 3. pub(crate) 表示该 mod 的 pub 行为只在当前 crate 生效 == pub(in crate)
+//    同理还有 pub(super), pub(in super), pub(self), pub(in self)
 pub(crate) mod basic_config;
 pub(crate) mod error;
 pub(crate) mod poem_config;
